914936355546703566ae0e973f6c28dd4ce6d84f
Xavier Goss <xgoss@live.com>
2025-06-08 21:58:13 -0400

    Added the ability to add events

--- MembershipManager.Client/src/pages/index.tsx
+++ MembershipManager.Client/src/pages/index.tsx
@@ -1,16 +1,31 @@
 import Calendar, { Event as CalendarEvent } from "@/components/Calendar";
+import CreateSidebar from "@/components/CreateSidebar";
+import EditSidebar from "@/components/EditSidebar";
 import Layout from "@/components/Layout";
-import { QueryEvent, Event } from "@/dtos";
-import { useClient } from "@/gateway";
+import { QueryEvent, Event, CreateEvent, EventType, State } from "@/dtos";
+import { useApp, useClient } from "@/gateway";
+import { FormInput, InputType } from "@/types";
 import { useEffect, useState } from "react";
 
 const Index = () => {
 	const client = useClient();
+	const app = useApp();
 
+	const [newEvent, setNewEvent] = useState<boolean>(false);
 	const [events, setEvents] = useState<CalendarEvent[]>([]);
 	useEffect(() => {
 		(async () => await refreshEvents())();
 	}, []);
+
+	const reset = (args: { newEvent?: boolean; editEventId?: number } = {}) => {
+		setNewEvent(args.newEvent ?? false);
+	};
+
+	const onDone = async () => reset();
+	const onSave = async () => {
+		onDone();
+		await refreshEvents();
+	};
 
 	const refreshEvents = async () => {
 		const api = await client.api(new QueryEvent({}));
@@ -26,6 +41,7 @@ const Index = () => {
 	};
 
 	const handleAddEvent = async (date: Date) => {
+		setNewEvent(true);
 		await refreshEvents();
 		// Navigate to external page for adding event, pass date as query param
 	};
@@ -35,6 +51,81 @@ const Index = () => {
 		// Navigate to external page for editing event by id
 	};
 
+	const createInputs: FormInput[] = [
+		{
+			type: InputType.Select,
+			id: "eventType",
+			options: app.enumOptions("EventType"),
+			value: (dto: CreateEvent) => {
+				return dto.eventType;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.eventType = value as EventType;
+			},
+		},
+		{
+			type: InputType.TextInput,
+			id: "description",
+			value: (dto: CreateEvent) => {
+				return dto.description;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.description = value as string;
+			},
+		},
+		{
+			type: InputType.DateTime,
+			id: "dateTime",
+			value: (dto: CreateEvent) => {
+				return dto.dateTime;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.dateTime = value as string;
+			},
+		},
+		{
+			type: InputType.TextInput,
+			id: "address",
+			value: (dto: CreateEvent) => {
+				return dto.address;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.address = value as string;
+			},
+		},
+		{
+			type: InputType.TextInput,
+			id: "city",
+			value: (dto: CreateEvent) => {
+				return dto.city;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.city = value as string;
+			},
+		},
+		{
+			type: InputType.Select,
+			id: "state",
+			options: app.enumOptions("State"),
+			value: (dto: CreateEvent) => {
+				return dto.state;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.state = value as State;
+			},
+		},
+		{
+			type: InputType.TextInput,
+			id: "zipCode",
+			value: (dto: CreateEvent) => {
+				return dto.zipCode;
+			},
+			onChange: (dto: CreateEvent, value: string) => {
+				dto.zipCode = value as string;
+			},
+		},
+	];
+
 	return (
 		<Layout title="React SPA with Vite + TypeScript">
 			<div className="mt-5">
@@ -43,6 +134,15 @@ const Index = () => {
 					onAddEvent={handleAddEvent}
 					onEditEvent={handleEditEvent}
 					className="max-w-5xl mx-auto"
+				/>
+				<CreateSidebar
+					typeName="Events"
+					open={newEvent}
+					inputs={createInputs}
+					onDone={onDone}
+					onSave={onSave}
+					instance={() => new CreateEvent()}
+					create={(request: any) => new CreateEvent(request)}
 				/>
 			</div>
 		</Layout>
