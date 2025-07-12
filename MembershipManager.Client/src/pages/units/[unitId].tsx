294cd5ef86b746f618832564ed1d32f71a2f0743
Xavier Goss <xgoss@live.com>
2025-07-12 18:35:24 -0400

    Move around page structure

---
+++ MembershipManager.Client/src/pages/units/[unitId].tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const UnitPage = () => {
+	return (
+		<Layout title="Unit">
+			<p>Unit Page Coming soon...</p>
+		</Layout>
+	);
+};
+
+export default UnitPage;
