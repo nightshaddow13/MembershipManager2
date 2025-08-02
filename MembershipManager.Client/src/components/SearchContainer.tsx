febde75d98c2e24745def361a05f214dc820964e
Xavier Goss <xgoss@live.com>
2025-08-02 14:35:05 -0400

    Save schools list

--- MembershipManager.Client/src/components/SearchContainer.tsx
+++ MembershipManager.Client/src/components/SearchContainer.tsx
@@ -1,11 +1,24 @@
 import { useClient } from "@/gateway";
 import { useCallback, useEffect, useState } from "react";
 import { Input } from "./ui/input";
-import { IReturn, ISearch, QueryResponse } from "@/dtos";
+import { IReturn, ISearch } from "@/dtos";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { LucideProps } from "lucide-react";
 
 type SearchConstructor<T extends ISearch> = new () => T;
 
-export class SearchCardInfo {}
+export class SearchCardStatistic {
+	public icon?: React.ComponentType<LucideProps>;
+	public text: string = "";
+	public statistic: number = 0;
+}
+
+export class SearchCardInfo {
+	public key: number = 0;
+	public onClick: (key: number) => void = (key) => {};
+	public title: string = "";
+	public statistics: SearchCardStatistic[] = [];
+}
 
 type Props<TThing, TSearch extends ISearch & IReturn<TThing[]>> = {
 	SearchClass: SearchConstructor<TSearch>;
@@ -37,7 +50,6 @@ function SearchContainer<TThing, TSearch extends ISearch & IReturn<TThing[]>>({
 			const api = await client.api(query);
 			if (api.succeeded) {
 				setThings(api.response ?? []);
-				console.info(things);
 			}
 		},
 		[client]
@@ -55,9 +67,42 @@ function SearchContainer<TThing, TSearch extends ISearch & IReturn<TThing[]>>({
 			/>
 
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-				{things.map((thing) => (
-					<p>thing</p>
-				))}
+				{things.map((thing) => {
+					const cardInfo = mapToCard(thing);
+					return (
+						<Card
+							key={cardInfo.key}
+							onClick={() => cardInfo.onClick(cardInfo.key)}
+							className="cursor-pointer hover:shadow-lg transition-shadow duration-200"
+							role="button"
+							tabIndex={0}
+							onKeyDown={(e) => {
+								if (e.key === "Enter" || e.key === " ") {
+									cardInfo.onClick(cardInfo.key);
+								}
+							}}
+						>
+							<CardHeader>
+								<CardTitle>{cardInfo.title}</CardTitle>
+								<CardContent className="flex flex-col justify-between flex-grow space-y-2">
+									{cardInfo.statistics.map((statistic, index) => (
+										<div
+											key={index}
+											className="flex items-center space-x-2"
+										>
+											{statistic.icon && (
+												<statistic.icon className="w-5 h-5 text-muted-foreground" />
+											)}
+											<span>
+												test {statistic.statistic} {statistic.text}
+											</span>
+										</div>
+									))}
+								</CardContent>
+							</CardHeader>
+						</Card>
+					);
+				})}
 			</div>
 		</div>
 	);
--- MembershipManager.Client/src/pages/schools/index.tsx
+++ MembershipManager.Client/src/pages/schools/index.tsx
@@ -1,12 +1,44 @@
 import Page from "@/components/LayoutPage";
-import SearchContainer from "@/components/SearchContainer";
-import { SearchSchools } from "@/dtos";
+import SearchContainer, {
+	SearchCardInfo,
+	SearchCardStatistic,
+} from "@/components/SearchContainer";
+import { School, SearchSchools } from "@/dtos";
 import { ValidateAuth } from "@/useAuth";
+import { Building, CalendarDays } from "lucide-react";
 
 function Index() {
+	function mapSchoolToCard(school: School) {
+		const stat1 = new SearchCardStatistic();
+		stat1.icon = CalendarDays;
+		stat1.statistic = 1;
+		stat1.text = "one";
+
+		const stat2 = new SearchCardStatistic();
+		stat2.icon = Building;
+		stat2.statistic = 2;
+		stat2.text = "Two";
+
+		const stats: SearchCardStatistic[] = [stat1, stat2];
+
+		const card = new SearchCardInfo();
+
+		card.key = school.id;
+		card.title = school.description;
+		card.onClick = (number: number) => {
+			console.log(number);
+		};
+		card.statistics = stats;
+
+		return card;
+	}
+
 	return (
 		<Page title="School Management">
-			<SearchContainer SearchClass={SearchSchools} />
+			<SearchContainer
+				SearchClass={SearchSchools}
+				mapToCard={mapSchoolToCard}
+			/>
 		</Page>
 	);
 }
